d8853ca977d7036d195f03dde8704e5327c79d2c
satpreet07 <satpreetvirdi007@gmail.com>
2024-11-12 04:13:31 +0530

    modified vercel.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,7 @@ const App = () => {
     setError('');
 
     try {
-      const response = await axios.post('/api/summarize', { videoUrl });
+      const response = await axios.post('https://yt-capsule.vercel.app/api/summarize', { videoUrl });
       setSummary(response.data.summary);
     } catch (err) {
       console.error('Error fetching summary:', err);
