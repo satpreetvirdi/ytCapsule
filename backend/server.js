09a6d7f63e6e7910b95283243ef93e69dbee2be8
satpreet07 <satpreetvirdi007@gmail.com>
2024-11-20 21:08:01 +0530

    fix: added redis 03

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.use(
     resave: false,
     saveUninitialized: true,
     cookie: {
-      secure: false,
+      secure: true,
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000
     },
